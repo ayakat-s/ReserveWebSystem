c8762c9555cf98b2ab752db2f119315aa1304ebf
ayakat-s <ayaka.tkhs14@gmail.com>
2020-04-12 23:59:25 +0900

    php

--- register.php
+++ register.php
@@ -41,24 +41,5 @@ if (count($err) === 0) {
 </body>
 </html>
 
-if (isset($_POST['signup'])) {
-    $userid = $_POST['userid']
-    $user_surname = $_POST['user_surname']
-    $user_givenname = $_POST['user_givenname']
-    $password = $_POST['password']
-    try {
-        $db = new PDO('mysql:host=localhost;dbname=sample' , '', '');
-        $sql = 'insert into users(userid, user_surname, user_givenuname, password) values(?, ?, ?, ?)';
-        $stmt = $db->prepare($sql);
-        $stmt->execute(array($userid, $user_surname, $user_givenuname, $password));
-        $stmt = null;
-        $db = null;
 
-        header('Location: '); //ホーム画面に遷移
-        exit;
-    } catch (PDOException $e) {
-        echo $e->getMessage();
-        exit;
-    }
-}
 
