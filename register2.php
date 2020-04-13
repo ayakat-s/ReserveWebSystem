5f1415ca9f5a2caa34e8b0d425c06bb4660ea094
ayakat-s <ayaka.tkhs14@gmail.com>
2020-04-13 22:28:34 +0900

    signup

--- register2.php
+++ register2.php
@@ -1,4 +1,7 @@
 <?php
+require 'smarty.php';
+$smarty = new customSmarty();
+$smarty->display("register2.php");
 
 $err = [];
 
@@ -21,4 +24,25 @@ if (count($err) === 0) {
     //ユーザを登録する処理
 }
 
+?>
+
+
+<!DOCTYPE html>
+<html lang="jp">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>ユーザ登録完了画面</title>
+</head>
+<body>
+    <?php if (count($err) > 0) : ?>
+        <?php foreach($err as $e) : ?>
+            <p><?php echo $e ?></p>
+        <?php endforeach ?>
+    <?php else : ?>
+        <p>ユーザ登録が完了しました。</p>
+    <?php endif ?> 
+    <a href="index.php">戻る</a>
+</body>
+</html>//
 
