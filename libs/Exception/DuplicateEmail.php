a2ac2747800c3198c09b5f5d4d11cada738d867d
ayakat-s <ayaka.tkhs14@gmail.com>
2020-04-17 01:00:01 +0900

    delete files

--- libs/Exception/DuplicateEmail.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-namespace MyApp\Exception;
-
-class DuplicateEmail extends \Exception {
-  protected $message = 'Duplicate Email!';
-}
--- libs/Exception/UnmatchEmailOrPassword.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-namespace MyApp\Exception;
-
-class UnmatchEmailOrPassword extends \Exception {
-  protected $message = 'Email/Password do not match!';
-}
