654b6d2061c7ce296da25b6cad88f7d7d7ceb43c
ayakat-s <ayaka.tkhs14@gmail.com>
2020-04-19 19:52:14 +0900

    set weekly calendar

--- templates_c/10764427240022c7072444d353879028f59c1a68_0.file.salonCalendar.tpl.php
+++ templates_c/10764427240022c7072444d353879028f59c1a68_0.file.salonCalendar.tpl.php
@@ -1,4 +1,27 @@
-<!DOCTYPE html>
+<?php
+/* Smarty version 3.1.33, created on 2020-04-19 12:44:50
+  from 'C:\Users\ayata\Desktop\seitai\template\salonCalendar.tpl' */
+
+/* @var Smarty_Internal_Template $_smarty_tpl */
+if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
+  'version' => '3.1.33',
+  'unifunc' => 'content_5e9c2ba28ee4c2_14609898',
+  'has_nocache_code' => false,
+  'file_dependency' => 
+  array (
+    '10764427240022c7072444d353879028f59c1a68' => 
+    array (
+      0 => 'C:\\Users\\ayata\\Desktop\\seitai\\template\\salonCalendar.tpl',
+      1 => 1587293088,
+      2 => 'file',
+    ),
+  ),
+  'includes' => 
+  array (
+  ),
+),false)) {
+function content_5e9c2ba28ee4c2_14609898 (Smarty_Internal_Template $_smarty_tpl) {
+?><!DOCTYPE html>
 <html lang="ja">
     <head>
         <!-- Required meta tags -->
@@ -8,15 +31,23 @@
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
         <!-- Optional JavaScript -->
         <!-- jQuery first, then Popper.js, then Bootstrap JS -->
-        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
-        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
-        <script>
+        <?php echo '<script'; ?>
+ src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"><?php echo '</script'; ?>
+>
+        <?php echo '<script'; ?>
+ src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"><?php echo '</script'; ?>
+>
+        <?php echo '<script'; ?>
+ src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"><?php echo '</script'; ?>
+>
+        <?php echo '<script'; ?>
+>
             $(function() {
                 $("#commonHeader").load("template/header.html");
                 $("#commonFooter").load("template/footer.html");
             });
-        </script>
+        <?php echo '</script'; ?>
+>
         <title>空き状況・ご予約｜整体サロン Natural</title>
         <link rel="stylesheet" href="../css/style.css">
     </head>
@@ -35,11 +66,27 @@
                     <table class="calendar">
                         <thead>
                             <tr>
-                                {foreach from=$week item=node}<th>{$node}</th>{/foreach}
+                                <?php
+$_from = $_smarty_tpl->smarty->ext->_foreach->init($_smarty_tpl, $_smarty_tpl->tpl_vars['week']->value, 'node');
+if ($_from !== null) {
+foreach ($_from as $_smarty_tpl->tpl_vars['node']->value) {
+?><th><?php echo $_smarty_tpl->tpl_vars['node']->value;?>
+</th><?php
+}
+}
+$_smarty_tpl->smarty->ext->_foreach->restore($_smarty_tpl, 1);?>
                                 
                                 </tr>
                                 <tr>
-                                {foreach from=$this_week item=node}<td>{$node}</td>{/foreach}
+                                <?php
+$_from = $_smarty_tpl->smarty->ext->_foreach->init($_smarty_tpl, $_smarty_tpl->tpl_vars['this_week']->value, 'node');
+if ($_from !== null) {
+foreach ($_from as $_smarty_tpl->tpl_vars['node']->value) {
+?><td><?php echo $_smarty_tpl->tpl_vars['node']->value;?>
+</td><?php
+}
+}
+$_smarty_tpl->smarty->ext->_foreach->restore($_smarty_tpl, 1);?>
                                 
                                 </tr>
                         </thead>
@@ -63,4 +110,5 @@
             <footer id="commonFooter"></footer>
         </div>
     </body>
-</html>
+</html><?php }
+}
